2d03b0c6a3d4f8769e92a87384ddc02816bf4bee
Baklay Petro <pustota1988@gmail.com>
2023-07-30 07:37:45 +0300

    createSchemasSanity

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,1 +1,12 @@
-export const schemaTypes = []
+import test from './test'
+
+import works from './works'
+import testimonials from './testimonials'
+import brands from './brands'
+import abouts from './abouts'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import contact from './contact'
+
+export const schemaTypes = [test, works, testimonials, brands, abouts, skills, workExperience, experiences, contact]
